6d5b139bc27bad62f8da1cbb39e5fbdaed9f5fce
tomosue <nikoniko04211970@gmail.com>
2022-10-04 15:02:20 +0900

    test

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const https = require("https")
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 3000
-const TOKEN = process.env.LINE_ACCESS_TOKEN
+const https = require("https");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (req, res) => {
-    res.sendStatus(200)
-    
-    /*const headers = {
+  res.sendStatus(200);
+
+  /*const headers = {
       "Content-Type": "application/json",
       "Authorization": "Bearer " + TOKEN
     }*/
-})
+});
 
-app.post("/webhook", function(req, res) {
-  res.send("HTTP POST request sent to the webhook URL!")
+app.post("/webhook", function (req, res) {
+  res.send("HTTP POST request sent to the webhook URL!");
   // ユーザーがボットにメッセージを送った場合、返信メッセージを送る
   if (req.body.events[0].type === "message") {
     // 文字列化したメッセージデータ
@@ -27,53 +29,53 @@ app.post("/webhook", function(req, res) {
       replyToken: req.body.events[0].replyToken,
       messages: [
         {
-          "type": "text",
-          "text": "Hello, user"
+          type: "text",
+          text: "Hello, user",
         },
         {
-          "type": "text",
-          "text": "May I help youuuuuuuu?"
+          type: "text",
+          text: "May I help youuuuuuuu?",
         },
         {
-          "type": "text",
-          "text": event.source.userId
-        }
-      ]
-    })
+          type: "text",
+          text: req.event.source.userId,
+        },
+      ],
+    });
 
     // リクエストヘッダー
     const headers = {
       "Content-Type": "application/json",
-      "Authorization": "Bearer " + TOKEN
-    }
+      Authorization: "Bearer " + TOKEN,
+    };
 
     // リクエストに渡すオプション
     const webhookOptions = {
-      "hostname": "api.line.me",
-      "path": "/v2/bot/message/reply",
-      "method": "POST",
-      "headers": headers,
-      "body": dataString
-    }
+      hostname: "api.line.me",
+      path: "/v2/bot/message/reply",
+      method: "POST",
+      headers: headers,
+      body: dataString,
+    };
 
     // リクエストの定義
     const request = https.request(webhookOptions, (res) => {
       res.on("data", (d) => {
-        process.stdout.write(d)
-      })
-    })
+        process.stdout.write(d);
+      });
+    });
 
     // エラーをハンドル
     request.on("error", (err) => {
-      console.error(err)
-    })
+      console.error(err);
+    });
 
     // データを送信
-    request.write(dataString)
-    request.end()
+    request.write(dataString);
+    request.end();
   }
-})
+});
 
 app.listen(PORT, () => {
-console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
